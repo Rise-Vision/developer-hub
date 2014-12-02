952368420b7a2cca95d5076c288a3871f3c81180
rodrigopavezi <rodrigopavezi@gmail.com>
2014-12-02 11:35:52 -0200

    Changed table style and fixed sorting.

--- web/scripts/api-explorer/controllers/ctr-api-explorer-list.js
+++ web/scripts/api-explorer/controllers/ctr-api-explorer-list.js
@@ -4,8 +4,26 @@
 "use strict";
 angular.module("risevision.developer.hub")
     .controller("ApiExplorerListController",
-    ["$scope", "$state", "$log", "getRest",
-        function($scope, $state, $log,getRest){
+    ["$rootScope", "$scope", "$state", "$log",
+        function($rootScope, $scope, $state, $log){
+            $scope.sortReverse = false;
+
+            $scope.sortBy = function(category){
+                $scope.sortByCat = category;
+                $scope.sortReverse = !$scope.sortReverse;
+            }
+
+            $scope.methods = [];
+            $scope.$watch(function () { return $rootScope.resources; },
+                function (resources) {
+                    for(var resource in resources){
+                        for(var method in resources[resource].methods){
+                            $scope.methods.push(resources[resource].methods[method]);
+                        }
+                    }
+                }
+            );
+
 
             $scope.explore = function(id) {
                 $state.go("api-explorer.explore.endpoint", {id: id});
